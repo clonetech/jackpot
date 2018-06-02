830d1b0337c39f85bbae2ea410bcf29f44979078
clonetech <kenkipkirui08@gmail.com>
2018-06-02 20:29:05 +0300

    make it better

--- kipkirui/settings.py
+++ kipkirui/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'jackpot.apps.JackpotConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
