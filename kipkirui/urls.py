5726d9b0abe1f389b8ce388b0a1f0bf95f6ec809
clonetech <kenkipkirui08@gmail.com>
2018-06-05 17:29:20 +0300

    make it better

--- kipkirui/urls.py
+++ kipkirui/urls.py
@@ -16,3 +16,8 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        url(r'^static/(?P<path>.*)$', serve, {
+            'document_root': settings.STATIC_ROOT,
+        }),
+    ]
