64786490b94e0c5940f75c1e453b39efd9d6175b
clonetech <kenkipkirui08@gmail.com>
2018-06-07 16:26:35 +0300

    make it better

--- kipkirui/settings.py
+++ kipkirui/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'kipkirui.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [**'C:\\Python27\\Lib\\site-packages\\django\\contrib\\admin\\templates\\admin'**],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
