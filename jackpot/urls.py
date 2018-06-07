63e55b22229d27bd69c9f2fbd5ad4564858019a9
clonetech <kenkipkirui08@gmail.com>
2018-06-07 17:38:50 +0300

    make it better

--- jackpot/urls.py
+++ jackpot/urls.py
@@ -16,7 +16,6 @@ urlpatterns = [
     path('payment/', views.payment, name='payment'),
     path('guide/', views.guide, name='guide'),
     path('vip_jp/', views.vip_jp, name='vip_jp'),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('accounts/', include('django.contrib.auth.urls')),
 
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
