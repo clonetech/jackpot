be554c1fe60ac7681d31d6adb7794963f46ce062
clonetech <kenkipkirui08@gmail.com>
2018-06-07 20:56:22 +0300

    make it better

--- kipkirui/settings.py
+++ kipkirui/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'lj@fpp$g64kxdtg_=)gr8t0(_7ill-qbraz2=h3$xddh2uw#$%')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
